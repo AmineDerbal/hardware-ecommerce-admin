74a72e38be7cf21b92745cda3f8752e0517aa25e
Amine derbal <aminedoran31@gmail.com>
2024-01-17 13:07:41 +0100

    Update App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from './redux/store';
 
 const App = () => {
+  const dispatch = useDispatch();
+  const { username } = useSelector((state: RootState) => state.user);
+  useEffect(() => {}, [username]);
   return <div>App</div>;
 };
 
