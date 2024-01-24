6f45832511e90ca60a3f158c1237e1b1d4f971ac
Amine derbal <aminedoran31@gmail.com>
2024-01-24 14:35:30 +0100

    Setup Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from './redux/store';
+import { Home, Login } from './pages';
 
 const App = () => {
   const dispatch = useDispatch();
-  const { username } = useSelector((state: RootState) => state.user);
-  useEffect(() => {}, [username]);
-  return <div>App</div>;
+  const { isAdmin } = useSelector((state: RootState) => state.user);
+  useEffect(() => {}, [isAdmin]);
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={isAdmin ? <Home /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/login"
+            element={<Login />}
+          />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
