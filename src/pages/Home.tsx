708c10bcbab748b620af5ef9b646b43bd9c6a61c
Amine derbal <aminedoran31@gmail.com>
2024-02-05 13:56:58 +0100

    Update home display

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,7 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from 'react-router-dom';
-import { Topbar } from '../components';
 
 const Home = () => {
-  return (
-    <div>
-      <Router>
-        <Topbar />
-        <Routes></Routes>
-      </Router>
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
