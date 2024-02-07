2046ceeb6435772a9c565bb8f5a686543c582ab5
Amine derbal <aminedoran31@gmail.com>
2024-02-07 13:58:14 +0100

    Create fetured component

---
+++ src/components/featuredInfo/FeaturedInfo.tsx
@@ -0,0 +1,70 @@
+import './featuredInfo.css';
+import { ArrowDownward, ArrowUpward } from '@material-ui/icons';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const FeaturedInfo = () => {
+  const [income, setIncome] = useState([]);
+  const [perc, setPerc] = useState(0);
+  const persistRoot = localStorage.getItem('persist:root');
+  let tokenString: string;
+  const userString = JSON.parse(persistRoot as string);
+  const user = JSON.parse(userString);
+  tokenString = user.accessToken;
+  const userRequest = axios.create({
+    baseURL: process.env.REACT_APP_API_URL,
+    headers: { token: `Bearer ${tokenString}` },
+  });
+  useEffect(() => {
+    const getIncome = async () => {
+      try {
+        const res = await userRequest.get('orders/income');
+        setIncome(res.data);
+        setPerc((res.data[1].total * 100) / res.data[0].total - 100);
+      } catch {}
+    };
+    getIncome();
+  }, []);
+
+  return (
+    <div className="featured">
+      <div className="featuredItem">
+        <span className="featuredTitle">Revanue</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">${income[1]?.total}</span>
+          <span className="featuredMoneyRate">
+            %{Math.floor(perc)}{' '}
+            {perc < 0 ? (
+              <ArrowDownward className="featuredIcon negative" />
+            ) : (
+              <ArrowUpward className="featuredIcon" />
+            )}
+          </span>
+        </div>
+        <span className="featuredSub">Compared to last month</span>
+      </div>
+      <div className="featuredItem">
+        <span className="featuredTitle">Sales</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">$4,415</span>
+          <span className="featuredMoneyRate">
+            -1.4 <ArrowDownward className="featuredIcon negative" />
+          </span>
+        </div>
+        <span className="featuredSub">Compared to last month</span>
+      </div>
+      <div className="featuredItem">
+        <span className="featuredTitle">Cost</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">$2,225</span>
+          <span className="featuredMoneyRate">
+            +2.4 <ArrowUpward className="featuredIcon" />
+          </span>
+        </div>
+        <span className="featuredSub">Compared to last month</span>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedInfo;
