063532d31f9bc53bb979a3799ea3f98badbb3d0a
Amine derbal <aminedoran31@gmail.com>
2024-02-11 13:55:45 +0100

    Add styling

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -7,10 +7,10 @@ import {
   ResponsiveContainer,
 } from 'recharts';
 
-const Chart = ({ title, data, dataKey, grid }) => {
+const Chart = ({ title, data, dataKey, grid }: any) => {
   return (
     <div className="chart">
-      <h3 className="chartTitle">{title}</h3>
+      <h3 className="mb-[20px]">{title}</h3>
       <ResponsiveContainer
         width="100%"
         aspect={4 / 1}
