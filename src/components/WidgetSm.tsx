9fefa77ee0dd124b0d3cd0886762390c1fa6c238
Amine derbal <aminedoran31@gmail.com>
2024-02-14 14:26:13 +0100

    Create widget mobile file

---
+++ src/components/WidgetSm.tsx
@@ -0,0 +1,8 @@
+import { Visibility } from '@material-ui/icons';
+import { useEffect, useState } from 'react';
+
+const WidgetSm = () => {
+  return <div>WidgetSm</div>;
+};
+
+export default WidgetSm;
