41e13d4f6eeb95ff76bd4d65d011a3a471bec054
Amine derbal <aminedoran31@gmail.com>
2024-02-19 13:40:01 +0100

    Delete canceled features files

--- src/components/WidgetSm.tsx
+++
@@ -1,8 +0,0 @@
-import { Visibility } from '@material-ui/icons';
-import { useEffect, useState } from 'react';
-
-const WidgetSm = () => {
-  return <div>WidgetSm</div>;
-};
-
-export default WidgetSm;
