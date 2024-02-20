169105e0adf927f0e0bec53a72d987b00daa7550
Amine derbal <aminedoran31@gmail.com>
2024-02-20 14:19:20 +0100

    Update users List

--- src/pages/UserList.tsx
+++ src/pages/UserList.tsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Action, ThunkDispatch } from '@reduxjs/toolkit';
 import { useEffect } from 'react';
+import { DataGrid } from '@material-ui/data-grid';
 import { fetchUsers } from '../redux/usersList/usersListSlice';
 import { RootState } from '../redux/store';
 
@@ -10,13 +11,37 @@ const UserList = () => {
     (state: RootState) => state.usersList,
   );
   const { accessToken } = useSelector((state: RootState) => state.user);
+  const mappedUsers = users.map((user) => ({ ...user, id: user._id }));
   useEffect(() => {
-    if (accessToken !== null) {
-      dispatch(fetchUsers(accessToken));
+    if (!accessToken) {
+      return;
     }
+
+    dispatch(fetchUsers(accessToken));
   }, [dispatch, accessToken]);
   console.log(users);
-  return <div>UserList</div>;
+  const columns = [
+    { field: '_id', headerName: 'ID', width: 90 },
+    {
+      field: 'username',
+      headerName: 'Username',
+      width: 200,
+    },
+    {
+      field: 'email',
+      headerName: 'Email',
+      width: 200,
+    },
+  ];
+  return (
+    <div>
+      <DataGrid
+        rows={mappedUsers}
+        columns={columns}
+        disableSelectionOnClick
+      />
+    </div>
+  );
 };
 
 export default UserList;
